a12a80f1ff8d8826d5d02cc8e0bb845c77ab6653
Daniela Ramirez <angie.dramirez15@gmail.com>
2026-05-14 18:52:28 -0500

    actualizando proyecto

--- Backend/src/main/java/com/pastelarte/pastelarte_api/repository/ProductoRepository.java
+++ Backend/src/main/java/com/pastelarte/pastelarte_api/repository/ProductoRepository.java
@@ -4,4 +4,6 @@ import com.pastelarte.pastelarte_api.entities.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    List<Producto> findByCategoria(String categoria);
+    List<Producto> findByDisponibleTrue();
 }
