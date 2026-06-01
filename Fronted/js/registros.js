68c2898b456992d1eb975cdc6f4fcc01a66c49bb
Daniela Ramirez <angie.dramirez15@gmail.com>
2026-06-01 10:44:10 -0500

    conectando registro con bd

---
+++ Fronted/js/registros.js
@@ -0,0 +1,30 @@
+// VARIABLES GLOBALES
+
+// PRODUCTOS
+
+const listaProductos =
+    document.getElementById("listaProductos");
+
+const btnVerProductos =
+    document.getElementById("btnVerProductos");
+
+const btnAgregar =
+    document.getElementById("btnAgregar");
+
+const formularioProducto =
+    document.getElementById("formularioProducto");
+
+const guardarProducto =
+    document.getElementById("guardarProducto");
+
+// PEDIDOS
+
+const listaPedidos =
+    document.getElementById("listaPedidos");
+
+const btnVerPedidos =
+    document.getElementById("btnVerPedidos");
+
+// VARIABLE GLOBAL EDITAR
+
+let idEditando = null;
